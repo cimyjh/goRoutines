d188c07cd07ee70dd87c389132f2bec4af13695c
cimyjh <cimyjh@gmail.com>
2020-07-15 11:55:54 +0900

    goRoutines

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "japanguy", "larry", "abqcawf"}
+
+	for _, person := range people {
+		go isHello(person, c)
+	}
+
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func isHello(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + "is Hello"
+}
